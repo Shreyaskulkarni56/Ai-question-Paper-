779b9c8f02827b2d96dcfa6dc768a476c1a0a12e
Shreyas Kulkarni <135884938+Shreyaskulkarni56@users.noreply.github.com>
2026-09-01 19:41:16 +0530

    security: remove exposed Gemini API key test script

--- backend/list-models.js
+++
@@ -1,5 +0,0 @@
-const apiKey = "AIzaSyDZdKwpXwMJdSaArfXY1kDNRC7xm9lUhG8";
-fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${apiKey}`)
-  .then(res => res.json())
-  .then(data => console.log(JSON.stringify(data, null, 2)))
-  .catch(err => console.error(err));
